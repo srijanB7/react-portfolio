bb823b700b21a684f128862957f0a279577bdcf3
srijanbiswas7 <srijanbiswas955@gmail.com>
2023-07-24 13:38:22 +0530

    updated-image

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -4,14 +4,14 @@ export const projectData = [
         title: "Bettermart",
         desc: "Ecommerce website for clothes",
         stack: "React JS, CSS, React-Router, Mirage js",
-        img: "src/assets/ecommerce.png"
+        img: "https://res.cloudinary.com/dcxnaihyu/image/upload/v1690186029/ecommerce_skzfe1.png"
     },
     {
         id: 2,
         title: "Link Up",
         desc: "Social Media website",
         stack: "React JS, CSS, React-Router, Mirage js",
-        img: "src/assets/socialmedia.png"
+        img: "https://res.cloudinary.com/dcxnaihyu/image/upload/v1690186028/socialmedia_dxayni.png"
     },
 
 ]
