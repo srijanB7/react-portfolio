70f71cc08494a96c9cc10ccec96e6e8afe67b480
srijan <srijan@Apples-MacBook-Air.local>
2023-11-02 21:40:53 +0530

    added wordle project

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -1,21 +1,29 @@
 export const projectData = [
-    {
-        id: 1,
-        title: "Bettermart",
-        desc: "Ecommerce website for clothes",
-        stack: "React JS, CSS, React-Router, Mirage js",
-        img: "https://res.cloudinary.com/dcxnaihyu/image/upload/v1690186029/ecommerce_skzfe1.png",
-        githubLink: "https://github.com/srijanB7/bettermart/tree/main/src",
-        liveLink: "https://bettermart-ten.vercel.app/"
-    },
-    {
-        id: 2,
-        title: "Link Up",
-        desc: "Social Media website",
-        stack: "React JS, CSS, React-Router, Mirage js",
-        img: "https://res.cloudinary.com/dcxnaihyu/image/upload/v1690186028/socialmedia_dxayni.png",
-        githubLink: "https://github.com/srijanB7/linkup",
-        liveLink: "https://linkup-dusky.vercel.app"
-    },
-
-]
+  {
+    id: 1,
+    title: "Bettermart",
+    desc: "Ecommerce website for clothes",
+    stack: "React JS, CSS, React-Router, Mirage js",
+    img: "https://res.cloudinary.com/dcxnaihyu/image/upload/v1690186029/ecommerce_skzfe1.png",
+    githubLink: "https://github.com/srijanB7/bettermart/tree/main/src",
+    liveLink: "https://bettermart-ten.vercel.app/",
+  },
+  {
+    id: 2,
+    title: "Link Up",
+    desc: "Social Media website",
+    stack: "React JS, CSS, React-Router, Mirage js",
+    img: "https://res.cloudinary.com/dcxnaihyu/image/upload/v1690186028/socialmedia_dxayni.png",
+    githubLink: "https://github.com/srijanB7/linkup",
+    liveLink: "https://linkup-dusky.vercel.app",
+  },
+  {
+    id: 3,
+    title: "wordle",
+    desc: "Clone of the popular wordle game",
+    stack: "React JS, CSS",
+    img: "https://res.cloudinary.com/dcxnaihyu/image/upload/v1698940945/wordle.png",
+    githubLink: "https://github.com/srijanB7/wordle",
+    liveLink: "https://wordle-srijanb7.vercel.app/",
+  },
+];
