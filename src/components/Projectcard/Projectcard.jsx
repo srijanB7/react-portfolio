9828f7cbbd4279ba9a8ec26009455ef13b3bd7c1
srijan <srijan@Apples-MacBook-Air.local>
2023-11-02 21:41:36 +0530

    remove console log

--- src/components/Projectcard/Projectcard.jsx
+++ src/components/Projectcard/Projectcard.jsx
@@ -2,22 +2,32 @@ import { AiFillGithub, AiOutlineLink } from "react-icons/ai";
 
 import "./Projectcard.css";
 
-export const Projectcard = ({ img, desc, title, stack, githubLink, liveLink }) => {
-    //console.log(img);
-    return (
-        <div className="card-container">
-            <div className="top">
-                <img className="project-image" src={img} alt="project" />
-            </div>
-            <div className="bottom">
-                <h3>{title}</h3>
-                <p>{desc}</p>
-                <p>{stack}</p>
-            </div>
-            <div className="links">
-                <a href={githubLink}><AiFillGithub size="20px" className="link"/></a>
-                <a href={liveLink}><AiOutlineLink size="20px" className="link"/></a>
-            </div>
-        </div>
-    );
+export const Projectcard = ({
+  img,
+  desc,
+  title,
+  stack,
+  githubLink,
+  liveLink,
+}) => {
+  return (
+    <div className="card-container">
+      <div className="top">
+        <img className="project-image" src={img} alt="project" />
+      </div>
+      <div className="bottom">
+        <h3>{title}</h3>
+        <p>{desc}</p>
+        <p>{stack}</p>
+      </div>
+      <div className="links">
+        <a href={githubLink}>
+          <AiFillGithub size="20px" className="link" />
+        </a>
+        <a href={liveLink}>
+          <AiOutlineLink size="20px" className="link" />
+        </a>
+      </div>
+    </div>
+  );
 };
